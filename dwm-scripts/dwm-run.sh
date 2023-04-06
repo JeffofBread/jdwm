7d9fe6aa584b09bbf1dbdd1d10ee7c86ea38396f
JeffofBread <virginwyatt@gmail.com>
2023-04-05 21:45:39 -0400

    minor bind and dwm-run.sh fixes

--- dwm-scripts/dwm-run.sh
+++ dwm-scripts/dwm-run.sh
@@ -10,9 +10,9 @@ solaar -w hide &
 /usr/lib/kdeconnectd &
 picom &
 dwmblocks &
-feh --bg-scale ~/Pictures/Wallpapers/1.png ~/Pictures/Wallpapers/2.jpg ~/Pictures/Wallpapers/3.jpg &
 # My personal screen layout
 xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output HDMI-1 --mode 3440x1440 --pos 4480x0 --rotate normal --output HDMI-2 --mode 1920x1080 --pos 0x180 --rotate normal --output DP-4 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-5 --off &
+feh --bg-scale ~/Pictures/Wallpapers/1.png ~/Pictures/Wallpapers/2.jpg ~/Pictures/Wallpapers/3.jpg &
 
 while type dwm >/dev/null; do dwm && continue || break; done
 # dwm
