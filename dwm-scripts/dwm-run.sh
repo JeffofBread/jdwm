9d6c57e9ae0d4e50bf4353c274335a14d290910f
JeffofBread <virginwyatt@gmail.com>
2023-04-06 19:36:53 -0400

    fixed gimp crashing dwm, fixed wallpapers

--- dwm-scripts/dwm-run.sh
+++ dwm-scripts/dwm-run.sh
@@ -12,7 +12,7 @@ picom &
 dwmblocks &
 # My personal screen layout
 xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output HDMI-1 --mode 3440x1440 --rate 60.00 --pos 4480x0 --rotate normal --output HDMI-2 --mode 1920x1080 --rate 75.00 --pos 0x180 --rotate normal --output DP-4 --primary --mode 2560x1440 --rate 240.00 --pos 1920x0 --rotate normal --output DP-5 --off &
-feh --bg-scale ~/Pictures/Wallpapers/1.png ~/Pictures/Wallpapers/2.jpg ~/Pictures/Wallpapers/3.jpg &
+feh --bg-scale ~/Pictures/Wallpapers/main.jpg ~/Pictures/Wallpapers/ultrawide.jpg ~/Pictures/Wallpapers/main.jpg &
 
 while type dwm >/dev/null; do dwm && continue || break; done
 # dwm
