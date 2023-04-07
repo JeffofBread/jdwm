7e736f2832e20d61c5c8c5541e59a94f712c3b75
JeffofBread <virginwyatt@gmail.com>
2023-04-07 04:18:17 -0400

    moved scripts, updated make file

--- dwm-src/dwm-scripts/recompile.sh
+++ dwm-src/dwm-scripts/recompile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-kitty --hold sh -c "cd ${SCRIPT_DIR} && cd .. && cd dwm-src && sudo make clean install"
+kitty --hold sh -c "cd ${SCRIPT_DIR} && cd .. && sudo make clean install"
