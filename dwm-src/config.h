f4c894309e79dd7cab27611e7f42ddb954ccaae1
JeffofBread <virginwyatt@gmail.com>
2023-04-09 20:37:15 -0400

    bar padding added, need to fix systray's position

--- dwm-src/config.h
+++ dwm-src/config.h
@@ -16,23 +16,27 @@ static const unsigned int systrayonleft 	= 0;    	/* 0: systray in the right cor
 static const unsigned int systrayspacing 	= 2;   		/* systray spacing */
 static const int systraypinningfailfirst 	= 1;   		/* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        		= 1;        /* 0 means no systray */
-static const unsigned int gappih    		= 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    		= 20;       /* vert inner gap between windows */
-static const unsigned int gappoh    		= 20;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    		= 20;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    		= 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    		= 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    		= 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    		= 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          		= 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            		= 1;        /* 0 means no bar */
 static const int topbar             		= 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono:size=10" };
-static const char dmenufont[]       = "JetBrainsMono:size=10";
+static const int vertpad           			= 10;       /* vertical padding of bar */
+static const int sidepad            		= 10;       /* horizontal padding of bar */
+
+static const char *fonts[]          		= { "JetBrainsMono:size=10" };
+static const char dmenufont[]       		= "JetBrainsMono:size=10";
 
 // Default Color Scheme
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const char col_gray1[]       		= "#222222";
+static const char col_gray2[]       		= "#444444";
+static const char col_gray3[]       		= "#bbbbbb";
+static const char col_gray4[]       		= "#eeeeee";
+static const char col_cyan[]        		= "#005577";
+static const char *colors[][3] = 
+{
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
--- dwm-src/dwm.c
+++ dwm-src/dwm.c
@@ -306,6 +306,8 @@ static int screen;
 static int sw, sh;           /* X display screen geometry width, height */
 static int bh;               /* bar height */
 static int lrpad;            /* sum of left and right padding for text */
+static int vp;               /* vertical padding for bar */
+static int sp;               /* side padding for bar */
 static int (*xerrorxlib)(Display *, XErrorEvent *);
 static unsigned int numlockmask = 0;
 static void (*handler[LASTEvent]) (XEvent *) = {
@@ -910,7 +912,7 @@ drawbar(Monitor *m)
     if (m == selmon) { /* status is only drawn on selected monitor */
         drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext) - lrpad / 2 + 2; /* 2px extra right padding */
-		drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
+        drw_text(drw, m->ww - tw - stw - 2 * sp, 0, tw, bh, lrpad / 2 - 2, stext, 0);
     }
 
     resizebarwin(m);
@@ -936,12 +938,12 @@ drawbar(Monitor *m)
     if ((w = m->ww - tw - stw - x) > bh) {
         if (m->sel) {
             drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-            drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+            drw_text(drw, x, 0, w - 2 * sp, bh, lrpad / 2, m->sel->name, 0);
             if (m->sel->isfloating)
                 drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
         } else {
             drw_setscheme(drw, scheme[SchemeNorm]);
-            drw_rect(drw, x, 0, w, bh, 1, 1);
+            drw_rect(drw, x, 0, w - 2 * sp, bh, 1, 1);
         }
     }
     drw_map(drw, m->barwin, 0, 0, m->ww - stw, bh);
@@ -1588,10 +1590,10 @@ resize(Client *c, int x, int y, int w, int h, int interact)
 
 void
 resizebarwin(Monitor *m) {
-	unsigned int w = m->ww;
+	unsigned int w = m->ww -  2 * sp;
 	if (showsystray && m == systraytomon(m) && !systrayonleft)
 		w -= getsystraywidth();
-	XMoveResizeWindow(dpy, m->barwin, m->wx, m->by, w, bh);
+	XMoveResizeWindow(dpy, m->barwin, m->wx + sp, m->by + vp, w, bh);
 }
 
 void
@@ -1955,6 +1957,8 @@ setup(void)
         die("no fonts could be loaded.");
     lrpad = drw->fonts->h;
     bh = drw->fonts->h + 2;
+	sp = sidepad;
+	vp = (topbar == 1) ? vertpad : - vertpad;
     updategeom();
     /* init atoms */
     utf8string = XInternAtom(dpy, "UTF8_STRING", False);
@@ -2542,10 +2546,10 @@ updatebars(void)
     for (m = mons; m; m = m->next) {
         if (m->barwin)
             continue;
-        w = m->ww;
+        w = m->ww - 2 * sp;
 		if (showsystray && m == systraytomon(m))
 			w -= getsystraywidth();
-		m->barwin = XCreateWindow(dpy, root, m->wx, m->by, w, bh, 0, DefaultDepth(dpy, screen),
+		m->barwin = XCreateWindow(dpy, root, m->wx + sp, m->by + vp, w, bh, 0, DefaultDepth(dpy, screen),
                 CopyFromParent, DefaultVisual(dpy, screen),
                 CWOverrideRedirect|CWBackPixmap|CWEventMask, &wa);
         XDefineCursor(dpy, m->barwin, cursor[CurNormal]->cursor);
@@ -2562,11 +2566,11 @@ updatebarpos(Monitor *m)
     m->wy = m->my;
     m->wh = m->mh;
     if (m->showbar) {
-        m->wh -= bh;
-        m->by = m->topbar ? m->wy : m->wy + m->wh;
-        m->wy = m->topbar ? m->wy + bh : m->wy;
+		m->wh = m->wh - vertpad - bh;
+		m->by = m->topbar ? m->wy : m->wy + m->wh + vertpad;
+		m->wy = m->topbar ? m->wy + bh + vp : m->wy;
     } else
-        m->by = -bh;
+        m->by = -bh - vp;
 }
 
 void
