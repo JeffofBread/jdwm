22af1c90b8ce86ee56286ff883c41f6349cb13fb
JeffofBread <virginwyatt@gmail.com>
2023-08-07 23:28:02 -0400

    fixed name of recompile script

--- dwm-src/config/config.h
+++ dwm-src/config/config.h
@@ -111,7 +111,7 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[]      = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]       = { "kitty", NULL };
-static const char *recompilecmd[]  = { "jeff-dwm-recompile.sh", NULL };
+static const char *recompilecmd[]  = { "jeff_dwm-recompile.sh", NULL };
 static const char *jgmenucmd[]     = { "jgmenu_run", NULL };
 static const char  scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "kitty", "-T", scratchpadname, NULL };
