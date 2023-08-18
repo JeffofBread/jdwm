af3d3efe5f01c6670c81c92155972f45bbaf23d1
JeffofBread <virginwyatt@gmail.com>
2023-08-18 17:33:15 -0400

    fix monocle layout

--- dwm-src/vanitygaps.c
+++ dwm-src/vanitygaps.c
@@ -194,6 +194,22 @@ getfacts(Monitor *m, int msize, int ssize, float *mf, float *sf, int *mr, int *s
  * Bottomstack layout + gaps
  * https://dwm.suckless.org/patches/bottomstack/
  */
+
+void monocle(Monitor *m)
+{
+	unsigned int n;
+	int oh, ov, ih, iv;
+	Client *c;
+
+	getgaps(m, &oh, &ov, &ih, &iv, &n);
+
+	if (n > 0) /* override layout symbol */
+		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
+	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
+		resize(c, m->wx + ov, m->wy + oh, m->ww - 2 * c->bw - 2 * ov,
+		       m->wh - 2 * c->bw - 2 * oh, 0);
+}
+
 static void
 bstack(Monitor *m)
 {
