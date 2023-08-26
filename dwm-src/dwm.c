b5da529b690650a62a6882e24ccd3e832e4ca54a
JeffofBread <virginwyatt@gmail.com>
2023-08-26 18:12:57 -0400

    fix floating windows not centered on sendmon

--- dwm-src/dwm.c
+++ dwm-src/dwm.c
@@ -2474,6 +2474,8 @@ sendmon(Client *c, Monitor *m)
     detachstack(c);
     c->mon = m;
     c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+	c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
     attach(c);
     attachstack(c);
     setclienttagprop(c);
