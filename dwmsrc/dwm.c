4bfb124ed2a8b4f801bf189671fc54de60e57c31
JeffofBread <virginwyatt@gmail.com>
2024-03-07 04:14:29 -0500

    Theme updates

--- dwmsrc/dwm.c
+++ dwmsrc/dwm.c
@@ -87,7 +87,7 @@
 
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurSwal, CurLast }; /* cursor */
-enum { SchemeNorm, SchemeSel, SchemeStatus, SchemeTagsSel, SchemeTagsNorm, SchemeInfoSel, SchemeInfoNorm }; /* color schemes */
+enum { SchemeNorm, SchemeSel, SchemeSystray, SchemeStatus, SchemeTagsSel, SchemeTagsNorm, SchemeInfoSel, SchemeInfoNorm }; /* color schemes */
 enum { NetSupported, NetWMName, NetWMIcon, NetWMState, NetWMCheck,
        NetSystemTray, NetSystemTrayOP, NetSystemTrayOrientation, NetSystemTrayOrientationHorz,
        NetWMFullscreen, NetWMSticky, NetActiveWindow, NetWMWindowType,
@@ -827,7 +827,7 @@ clientmessage(XEvent *e)
 
 			XReparentWindow(dpy, c->win, systray->win, 0, 0);
 			/* use parents background color */
-			swa.background_pixel  = scheme[SchemeNorm][ColBg].pixel;
+			swa.background_pixel  = scheme[SchemeSystray][ColBg].pixel;
 			XChangeWindowAttributes(dpy, c->win, CWBackPixel, &swa);
 			sendevent(c->win, netatom[Xembed], StructureNotifyMask, CurrentTime, XEMBED_EMBEDDED_NOTIFY, 0 , systray->win, XEMBED_EMBEDDED_VERSION);
 			/* FIXME not sure if I have to send these events, too */
@@ -3960,7 +3960,7 @@ updatesystray(void)
 		systray->win = XCreateSimpleWindow(dpy, root, x - sp, m->by + vp, w, bh, 0, 0, scheme[SchemeSel][ColBg].pixel);
 		wa.event_mask        = ButtonPressMask | ExposureMask;
 		wa.override_redirect = True;
-		wa.background_pixel  = scheme[SchemeNorm][ColBg].pixel;
+		wa.background_pixel  = scheme[SchemeSystray][ColBg].pixel;
 		XSelectInput(dpy, systray->win, SubstructureNotifyMask);
 		XChangeProperty(dpy, systray->win, netatom[NetSystemTrayOrientation], XA_CARDINAL, 32,
 				PropModeReplace, (unsigned char *)&netatom[NetSystemTrayOrientationHorz], 1);
@@ -3980,7 +3980,7 @@ updatesystray(void)
 	}
 	for (w = 0, i = systray->icons; i; i = i->next) {
 		/* make sure the background color stays the same */
-		wa.background_pixel  = scheme[SchemeNorm][ColBg].pixel;
+		wa.background_pixel  = scheme[SchemeSystray][ColBg].pixel;
 		XChangeWindowAttributes(dpy, i->win, CWBackPixel, &wa);
 		XMapRaised(dpy, i->win);
 		w += systrayspacing;
@@ -3999,7 +3999,7 @@ updatesystray(void)
 	XMapWindow(dpy, systray->win);
 	XMapSubwindows(dpy, systray->win);
 	/* redraw background */
-	XSetForeground(dpy, drw->gc, scheme[SchemeNorm][ColBg].pixel);
+	XSetForeground(dpy, drw->gc, scheme[SchemeSystray][ColBg].pixel);
 	XFillRectangle(dpy, systray->win, drw->gc, 0, 0, w, bh);
 	XSync(dpy, False);
 }
--- dwmsrc/themes/jeffs_theme.h
+++ dwmsrc/themes/jeffs_theme.h
@@ -1,19 +1,22 @@
-// Jeff's custom color scheme
-static const char col_gray1[] = "#222222";
-static const char col_gray2[] = "#444444";
-static const char col_gray3[] = "#bbbbbb";
-static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[]  = "#005577";
+// Jeff's custom color scheme, very simple green scheme
+static const char greygreen[]   = "#2c4c3b";    // SchemeSel
+static const char normalgreen[] = "#253a29";    // Bar background
+static const char darkgreen[]   = "#182c25";    // SchemeNorm
+static const char bluegreen[]   = "#1e453e";    // Unused
+static const char grey[]        = "#455b55";    // Unused
+static const char white[]       = "#e1e0db";    // Text
+
 
 static const char *colors[][4] = {
-	/*               fg         bg         border     float */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2, col_cyan },
+    //               fg         bg          border      float
+    [SchemeNorm] = { darkgreen, darkgreen,  darkgreen,  darkgreen },    // Generic scheme
+    [SchemeSel]  = { greygreen, greygreen,  greygreen,  greygreen },    // Generic scheme for focused windows
 
-	// colorbar Patch
-	[SchemeStatus]  	= { col_gray3, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
-	[SchemeTagsSel]  	= { col_gray4, col_cyan,   "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
-	[SchemeTagsNorm]  	= { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-	[SchemeInfoSel]  	= { col_gray4, col_cyan,   "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
-	[SchemeInfoNorm]  	= { col_gray3, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+    // colorbar Patch
+    [SchemeSystray]     = { white,          normalgreen,      "#000000"  },   // Systray {text,background,not used but cannot be empty}
+    [SchemeStatus]      = { white,          normalgreen,      "#000000"  },   // Statusbar right {text,background,not used but cannot be empty}
+    [SchemeTagsSel]     = { white,          normalgreen,      "#000000"  },   // Tagbar left selected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm]    = { white,          normalgreen,      "#000000"  },   // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]     = { white,          normalgreen,      "#000000"  },   // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]    = { white,          normalgreen,      "#000000"  },   // infobar middle  unselected {text,background,not used but cannot be empty}
 };
