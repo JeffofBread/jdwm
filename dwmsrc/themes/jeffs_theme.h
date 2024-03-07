9c86089855d73774b8440a4c1c3dabc72a951737
JeffofBread <virginwyatt@gmail.com>
2024-03-07 05:12:42 -0500

    more theme updates

--- dwmsrc/themes/jeffs_theme.h
+++ dwmsrc/themes/jeffs_theme.h
@@ -1,16 +1,16 @@
 // Jeff's custom color scheme, very simple green scheme
-static const char greygreen[]   = "#2c4c3b";    // SchemeSel
+static const char greygreen[]   = "#2f4a34";    // SchemeSel
 static const char normalgreen[] = "#253a29";    // Bar background
-static const char darkgreen[]   = "#182c25";    // SchemeNorm
+static const char darkgreen[]   = "#111b13";    // SchemeNorm
 static const char bluegreen[]   = "#1e453e";    // Unused
-static const char grey[]        = "#455b55";    // Unused
+static const char grey[]        = "#25272a";    // Window background
 static const char white[]       = "#e1e0db";    // Text
 
 
 static const char *colors[][4] = {
-    //               fg         bg          border      float
-    [SchemeNorm] = { darkgreen, darkgreen,  darkgreen,  darkgreen },    // Generic scheme
-    [SchemeSel]  = { greygreen, greygreen,  greygreen,  greygreen },    // Generic scheme for focused windows
+    //               fg         bg      border      float
+    [SchemeNorm] = { darkgreen, grey,   darkgreen,  darkgreen },    // Generic scheme
+    [SchemeSel]  = { greygreen, grey,   greygreen,  greygreen },    // Generic scheme for focused windows
 
     // colorbar Patch
     [SchemeSystray]     = { white,          normalgreen,      "#000000"  },   // Systray {text,background,not used but cannot be empty}
