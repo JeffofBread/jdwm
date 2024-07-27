ab9f3e2eae9eb1c82c925ad3574fc7d61f94a8c6
JeffofBread <virginwyatt@gmail.com>
2024-07-27 07:52:26 -0400

    fixed xrandr command

--- dwm/config/autorun.def.h
+++ dwm/config/autorun.def.h
@@ -13,7 +13,7 @@ static const char *const autostart[] = {
     "jeff_dwm_java.sh", NULL, // Exports java vars to help them java apps play nice with dwm
 
     // My xrandr config, just spread across multiple lines
-    /* "xrandr", "--output", "DP-0", "--off", "--output", "DP-1", "--off", "--output DP-2", "--off" "--output", "DP-3", "--off",
+    /* "xrandr", "--output", "DP-0", "--off", "--output", "DP-1", "--off", "--output", "DP-2", "--off", "--output", "DP-3", "--off",
     "--output", "HDMI-0", "--off", "--output", "HDMI-1", "--mode", "1920x1080", "--rate", "75.00", "--pos", "0x180", "--rotate",
     "normal", "--output", "HDMI-2", "--off", "--output", "DP-4", "--primary", "--mode", "2560x1440", "--rate", "240.00",
     "--pos", "1920x0", "--rotate", "normal", "--output", "DP-5", "--off", NULL, */
