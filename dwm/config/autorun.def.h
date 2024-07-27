80fdea6ae79a825c2169293a89f49daa416ce1e5
JeffofBread <virginwyatt@gmail.com>
2024-07-26 23:58:31 -0400

    moved xrandr up the array

--- dwm/config/autorun.def.h
+++ dwm/config/autorun.def.h
@@ -12,6 +12,12 @@
 static const char *const autostart[] = {
     "jeff_dwm_java.sh", NULL, // Exports java vars to help them java apps play nice with dwm
 
+    // My xrandr config, just spread across multiple lines
+    /* "xrandr", "--output", "DP-0", "--off", "--output", "DP-1", "--off", "--output DP-2", "--off" "--output", "DP-3", "--off",
+    "--output", "HDMI-0", "--off", "--output", "HDMI-1", "--mode", "1920x1080", "--rate", "75.00", "--pos", "0x180", "--rotate",
+    "normal", "--output", "HDMI-2", "--off", "--output", "DP-4", "--primary", "--mode", "2560x1440", "--rate", "240.00",
+    "--pos", "1920x0", "--rotate", "normal", "--output", "DP-5", "--off", NULL, */
+
     // Below are examples, not necessary:
     //---------------------------------//
 	//"picom", "--animations", "-b", NULL,  // Compositor, I recommend https://github.com/FT-Labs/picom
@@ -22,11 +28,5 @@ static const char *const autostart[] = {
 	//"flameshot", NULL,  // Screenshot tray tool
 	//"pasystray", NULL,	// Simple pulse audio tray app
 	//"jetbrains-toolbox", "--minimize", NULL,  // Jetbrains toolbox app for jetbrains software
-
-    // My xrandr config, just spread across multiple lines
-    /* "xrandr", "--output", "DP-0", "--off", "--output", "DP-1", "--off", "--output DP-2", "--off" "--output", "DP-3", "--off",
-    "--output", "HDMI-0", "--off", "--output", "HDMI-1", "--mode", "1920x1080", "--rate", "75.00", "--pos", "0x180", "--rotate",
-    "normal", "--output", "HDMI-2", "--off", "--output", "DP-4", "--primary", "--mode", "2560x1440", "--rate", "240.00",
-    "--pos", "1920x0", "--rotate", "normal", "--output", "DP-5", "--off", NULL, */
 	NULL,
 };
