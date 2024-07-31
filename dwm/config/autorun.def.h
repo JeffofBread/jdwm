898c85bb24bddeb43d0342a2a173d45827ff84bd
JeffofBread <virginwyatt@gmail.com>
2024-07-31 12:18:52 -0400

    comment changes

--- dwm/config/autorun.def.h
+++ dwm/config/autorun.def.h
@@ -8,10 +8,13 @@
 //                                                                                          //
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-// For autostarting programs and scripts at dwm startup.
+// For autostarting programs and scripts at jeff_dwm startup. 
+// Make sure each argument is its own string, with commas between, 
+// and a NULL at the end followed by a comma. The end of the array
+// should always end 'NULL,'.
 static const char *const autostart[] = {
-    "jeff_dwm_java.sh", NULL,           // Exports java vars to help them java apps play nice with dwm
-    "jeff_dwm_autostarts.sh", NULL,     // Uses jeff_dwm_autostart.sh to start any programs you want at login
-    "dwmblocks", NULL,                  // Replaces the status text with info specified in blocks, check /jeff_dwm/dwmblocks/
-    NULL,
+    "jeff_dwm_java.sh", NULL,           // Exports java vars to help some java apps work better with jeff_dwm
+    "jeff_dwm_autostarts.sh", NULL,     // Runs jeff_dwm_autostart.sh to start any programs you want at login
+    "dwmblocks", NULL,                  // dwmblocks, replaces the status text with info specified using scripts. Check out `/jeff_dwm/dwmblocks/config/blocks.h`
+    NULL,                               // Defines the end of the array, do not remove. 
 };
