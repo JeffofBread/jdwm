84f5893b702ac97fa50067fab56c7d830e6b9fc4
JeffofBread <virginwyatt@gmail.com>
2024-08-06 18:47:45 -0400

    default to no blocks enabled

--- dwmblocks/config/blocks.def.h
+++ dwmblocks/config/blocks.def.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 //  Icon                Command                                                         Update Interval         Update Signal
-    { " Ram: ",         "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",      30,                     0 },   // Ram / Memory
-    { " ",              "date '+%B %d (%a) %H:%M'",                                     5,                      0 },   // Date (Month Day (Day Name) 24h-time)
-    { " CPU Usage: ",   "echo ''$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]%",       5,                      0 },   // Cpu usage
-    { " ",              "",                                                             0,                      0 },   // Trailing spacer
+    { "No blocks on, check dwmblocks/config/blocks.h | ", "",                             0,                      0 },   // Remove after customization
+    //{ " Ram: ",         "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",      30,                     0 },   // Ram / Memory
+    //{ " ",              "date '+%B %d (%a) %H:%M'",                                     5,                      0 },   // Date (Month Day (Day Name) 24h-time)
+    //{ " CPU Usage: ",   "echo ''$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]%",       5,                      0 },   // Cpu usage
+    //{ " ",              "",                                                             0,                      0 },   // Trailing spacer
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
