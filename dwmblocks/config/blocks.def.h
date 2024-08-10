3c9aa762dc9c6bad5a27670b24161efd6759dc04
JeffofBread <virginwyatt@gmail.com>
2024-08-10 05:21:15 -0400

    add bug comment for deb

--- dwmblocks/config/blocks.def.h
+++ dwmblocks/config/blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 //  Icon                Command                                                         Update Interval         Update Signal
     { "No blocks on, check dwmblocks/config/blocks.h | ", "",                             0,                      0 },   // Remove after customization
-    //{ " Ram: ",         "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",      30,                     0 },   // Ram / Memory
+    //{ " Ram: ",         "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",      30,                     0 },   // Ram / Memory (slightly bugged in deb based systems, issue with original dwmblocks I think)
     //{ " ",              "date '+%B %d (%a) %H:%M'",                                     5,                      0 },   // Date (Month Day (Day Name) 24h-time)
     //{ " CPU Usage: ",   "echo ''$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]%",       5,                      0 },   // Cpu usage
     //{ " ",              "",                                                             0,                      0 },   // Trailing spacer
